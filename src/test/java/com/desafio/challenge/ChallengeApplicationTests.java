ad5d9a2b29200972d217f6d3d33911776a1289a0
Lautaro Vergara Amodeo <lautarovergaramodeo@gmail.com>
2023-01-23 16:40:32 -0300

    Primer commit

---
+++ src/test/java/com/desafio/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
